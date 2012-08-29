3d4f1482818a1586491ff59e308c414bc6aba6a5
Jamon Holmgren <jamon@clearsightstudio.com>
2012-08-29 14:25:27 -0700

    Added a query object to allow chaining.
    Still in progress.

--- lib/ParseModel/Query.rb
+++ lib/ParseModel/Query.rb
@@ -1,20 +1,138 @@
 module ParseModel
   module Model
     module ClassMethods
-      class Query
-        def where(conditions = {}, &callback)
-          query = PFQuery.queryWithClassName(self.to_s)
+      def query
+        q = ParseModel::Query.new
+        q.klass = self
+      end
 
-          conditions.each do |key, value|
-            query.whereKey(key, equalTo: value)
-          end
+      def where(conditions = {}, &callback)
+        q = query
+        q.where(conditions, callback)
+        q
+      end
+      def limit(offset, number = nil)
+        q = ParseModel::Query.new
+        q.limit(offset, number)
+        q
+      end
+    end
+  end
 
-          query.findObjectsInBackgroundWithBlock (lambda { |items, error|
-            class_items = items.map! { |item| self.new(item) }
-            callback.call class_items, error
-          })
+  class Query
+    attr_accessor :klass
+
+    def initialize
+      @conditions = @negativeConditions = @ltConditions = @gtConditions = []
+      @limit = @offset = nil
+    end
+
+    def fetch(&callback)
+      query = PFQuery.queryWithClassName(self.klass.to_s)
+
+      @conditions.each do |key, value|
+        query.whereKey(key, equalTo: value)
+      end
+      @negativeConditions.each do |key, value|
+        query.whereKey(key, notEqualTo: value)
+      end
+      @ltConditions.each do |key, value|
+        query.whereKey(key, lessThan: value)
+      end
+      @gtConditions.each do |key, value|
+        query.whereKey(key, greaterThan: value)
+      end
+      first = true
+      @order.each do |field, direction|
+        if first
+          query.orderByAscending(field) if direction && direction == :asc
+          query.orderByDescending(field) if direction && direction == :desc
+          first = false
+        else
+          query.addAscendingOrder(field) if direction && direction == :asc
+          query.addDescendingOrder(field) if direction && direction == :desc
         end
       end
+
+      query.limit = @limit if @limit
+      query.skip = @offset if @offset
+
+      fetchAll(query) if @limit.nil? || @limit > 1
+
+      self
+    end
+
+    def fetchAll(query)
+      query.findObjectsInBackgroundWithBlock (lambda { |items, error|
+        modelItems = items.map! { |item| self.klass.new(item) }
+        callback.call modelItems, error
+      })
+    end
+
+    # Query methods
+    def where(conditions = {}, &callback)
+      conditions.each do |c, v|
+        @conditions << {c, v}
+      end
+      query(callback)
+      self
+    end
+
+    def all(&callback)
+
+      self
+    end
+
+    def first(&callback)
+
+      self
+    end
+
+    # Query parameter methods
+
+    def limit(offset, number = nil)
+      if number.nil?
+        number = offset
+        offset = 0
+      end
+      @offset = offset
+      @limit = number
+      self
+    end
+
+    def order(fields)
+      fields.each do |field, direction|
+        @order << {"#{field}": direction}
+      end
+      self
+    end
+
+    def eq(fields = {})
+      fields.each do |field, value|
+        @conditions << {"#{field}": value}
+      end
+      self
+    end
+
+    def notEq(fields = {})
+      fields.each do |field, value|
+        @negativeConditions << {"#{field}": value}
+      end
+      self
+    end
+
+    def lt(fields = {})
+      fields.each do |field, value|
+        @ltConditions << {"#{field}": value}
+      end
+      self
+    end
+
+    def gt(fields = {})
+      fields.each do |field, value|
+        @gtConditions << {"#{field}": value}
+      end
+      self
     end
   end
 end
